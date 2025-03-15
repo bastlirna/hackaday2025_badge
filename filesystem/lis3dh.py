984c110f28cff8e5d2da51d0ac03d12c8d439e65
Aaka Fosfor <fosfor.software@seznam.cz>
2025-03-15 19:04:44 +0100

    rewriting the Lis3dh class in an attempt to add the X/Y/Z readout and make it a bit more readable

--- filesystem/lis3dh.py
+++ filesystem/lis3dh.py
@@ -5,26 +5,44 @@ __version__ = '0.1.0'
 
 class Lis3dh():
     I2C_ADDRESS = 0x19
+    
+    CTRL_REG1_REG = 0x20
+    CTRL_REG2_REG = 0x21
+    CTRL_REG3_REG = 0x22
+    CTRL_REG4_REG = 0x23
+    
+    X_REG = 0x28
+    
+    FIFO_CTRL_REG = 0x2E
+    FIFO_SRC_REG = 0x2F
+
     LEFT_ADC_REG = 0x88
     RIGHT_ADC_REG = 0x8A
 
-    def _read_register(self, addr, reg, nbytes=1):
-        return self._i2c.readfrom_mem(addr, reg, nbytes)
+    def _read_register(self, reg, nbytes=1):
+        return self._i2c.readfrom_mem(self.I2C_ADDRESS, reg, nbytes)
 
-    def _read_register_expect(self, addr, reg, expected):
-        value = self._read_register(addr, reg)
+    def _read_register_expect(self, reg, expected, nbytes=1):
+        #value = int.from_bytes(self._read_register(reg, nbytes), "big")
+        value = self._read_register(reg, nbytes)
+        if value != expected:
+            raise RuntimeError("Value doesn't match!")
+            # Address: 0x%X, read: 0x%X, expected: 0x%X" % (reg, value, expected))
+
+    def _write_register(self, reg, data:Bytes):
+        self._i2c.writeto_mem(self.I2C_ADDRESS, reg, data)
+
+    def _write_register_expect(self, reg, data:Bytes):
+        self._write_register(reg, data)
+        self._read_register_expect(reg, data, len(data))
 
     def _init_lis3dh(self):
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x20, bytearray([0x07])) #
-        self._read_register_expect(self.I2C_ADDRESS, 0x20, 0x07)
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x20, bytearray([0x77])) #
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x23, bytearray([0x88])) #
-        self._read_register_expect(self.I2C_ADDRESS, 0x22, 0x10)
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x22, bytearray([0x10])) #
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x1F, bytearray([0x80])) #
-        self._read_register_expect(self.I2C_ADDRESS, 0x23,0x88)
-        self._i2c.writeto_mem(self.I2C_ADDRESS, 0x23, bytearray([0x88])) #
-        self._read_register_expect(self.I2C_ADDRESS, 0x23, 0x88)
+        self._write_register(0x1F, bytearray([0x80]))
+
+        self._write_register_expect(self.CTRL_REG1_REG, bytearray([0x77]))
+        self._write_register_expect(self.CTRL_REG3_REG, bytearray([0x10]))
+        self._write_register_expect(self.CTRL_REG4_REG, bytearray([0x88]))
+
         time.sleep_ms(100)
 
     def __init__(self, i2c_bus):
@@ -35,13 +53,13 @@ class Lis3dh():
         adc_registers = [self.LEFT_ADC_REG, self.RIGHT_ADC_REG]
         adc_values = []
         for reg in adc_registers:
-            data = self._i2c.readfrom_mem(self.I2C_ADDRESS, reg, 2)
+            data = self._read_register(reg, 2)
             value = (data[0] | (data[1] << 8))
             adc_values.append(value)
         return adc_values
 
     def _read_left(self):
-      dataL = ((self._read_register(self.I2C_ADDRESS, self.LEFT_ADC_REG, 2)))
+      dataL = ((self._read_register(self.LEFT_ADC_REG, 2)))
       left = (dataL[1]<<8)|dataL[0]
       if (left > 32512):
          left = left-65536
@@ -49,12 +67,21 @@ class Lis3dh():
       return (left // 508) # value from 0..128
       
     def _read_right(self):
-      dataR = ((self._read_register(self.I2C_ADDRESS, self.RIGHT_ADC_REG, 2)))
+      dataR = ((self._read_register(self.RIGHT_ADC_REG, 2)))
       right = (dataR[1]<<8)|dataR[0]
       if (right > 32512):
         right = right-65535
       right = right + 32512 # value from 0..65535
       return (right // 508) # value from 0..128
+   
+    def _read_x(self):
+      data = ((self._read_register(self.X_REG, 2)))
+      value = (data[1]<<8)|data[0]
+      return value
+      if (value > 32512):
+        value = value-65535
+      value = value + 32512 # value from 0..65535
+      return (value // 508) # value from 0..128
    
     @property
     def left(self):
@@ -64,6 +91,9 @@ class Lis3dh():
     def right(self):
         return self._read_right()
     
+    @property
+    def x(self):
+        return self._read_x()
     
 if __name__ == "__main__":
     # Initialize I2C
@@ -71,6 +101,7 @@ if __name__ == "__main__":
     
     lis = Lis3dh(i2c1)
     while True:
-        print (lis.left, lis.right)
+        print (lis.left, lis.right, lis.x)
+        time.sleep(0.100)
     
 
